01139c1dfe9a0495e9965fc0c775171f3a871aeb
Padraig Dooley <padraigdooley@gmail.com>
2023-10-11 21:29:22 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Vans from "./pages/Vans"
+import VanDetail from "./pages/VanDetail"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./server"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  /**
+   * Challenge: set up the code so we can use a layout route!
+   * 
+   * 1. Create a folder called "components"
+   * 2. Create 2 new component files: Layout.jsx and Header.jsx
+   * 3. Move the <header> code below to the Header component file. 
+   *    (DON'T import that Header component here!)
+   */
+  return (
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(<App />);
